25f6b546fc85e60b8668c6c44fa86a7085168355
Terrencew867@gmail.com <Terrencew867@gmail.com>
2025-08-13 12:32:22 -0500

    Responsive styling to all css files

--- gallery.js
+++ gallery.js
@@ -5,15 +5,14 @@ const subscribeBtn = document.getElementById("subscribe-btn");
 const emailInput = document.getElementById("email-input");
 
 
-const addToCart  = () => { 
-    alert("Item added to cart");
-    
+const addToCart = () => { 
+   alert("Item added to cart!");
 }
 
 const sub = () => { 
     const email = emailInput.ariaValueMax.trim();
     if (email) { 
-        alert("Subscribed with: " + email);
+        alert(`Subscribed with: ${email}`);
         emailInput.value = "";
     } else { 
         alert("Please enter your email address")
